8ebdfe481fd28578b6c4beb7891b6bed81b3dd47
John Berberich <jberberich@gmail.com>
2022-03-31 21:39:42 -0400

    VNDLY-23014: Add support for Django 3.0 (#1)

--- tenant_schemas/management/commands/__init__.py
+++ tenant_schemas/management/commands/__init__.py
@@ -3,11 +3,6 @@ from django.conf import settings
 from django.core.management import call_command, get_commands, load_command_class
 from django.core.management.base import BaseCommand, CommandError
 from django.db import connection
-
-try:
-    from django.utils.six.moves import input
-except ImportError:
-    input = raw_input
 from tenant_schemas.utils import get_tenant_model, get_public_schema_name
 
 
